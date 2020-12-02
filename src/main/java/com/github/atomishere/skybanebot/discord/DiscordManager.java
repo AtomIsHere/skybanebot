015799a8458656e38317c0f2970d5b75567e1601
AtomIsHere <archietomoconnor@gmail.com>
2020-12-02 22:45:10 +1100

    Add code to connect to the Discord API

--- src/main/java/com/github/atomishere/skybanebot/discord/DiscordManager.java
+++ src/main/java/com/github/atomishere/skybanebot/discord/DiscordManager.java
@@ -18,20 +18,44 @@
 package com.github.atomishere.skybanebot.discord;
 
 import com.github.atomishere.skybanebot.SkybaneBot;
-import com.github.atomishere.skybanebot.cache.guild.GuildCache;
 import com.github.atomishere.skybanebot.config.ConfigurationValue;
-import com.github.atomishere.skybanebot.discord.commands.GetInactiveMembersCommand;
-import com.github.atomishere.skybanebot.discord.commands.RegisterInactivityCommand;
 import com.github.atomishere.skybanebot.service.AbstractService;
+import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.jagrosh.jdautilities.command.CommandClient;
+import com.neovisionaries.ws.client.DualStackMode;
+import com.neovisionaries.ws.client.WebSocketFactory;
+import lombok.Getter;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.ShutdownEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.concurrent.*;
+import java.util.logging.Logger;
 
 public class DiscordManager extends AbstractService {
-    private static final String OWNER_ID = "332423993132974081";
+    private static final Logger logger =  Logger.getLogger(DiscordManager.class.getName());
 
-    private CommandClient client;
+    private static final String OWNER_ID = "332423993132974081";
+    private static final EnumSet<GatewayIntent> intents = EnumSet.of(
+            GatewayIntent.GUILD_MEMBERS,
+            GatewayIntent.GUILD_EMOJIS,
+            GatewayIntent.GUILD_MESSAGES
+    );
+
+    @Getter
+    private JDA jda;
+    private CommandClient commandClient;
 
     @ConfigurationValue
-    private int requiredXp = 25000;
+    private String botToken = "bot-token-here";
+
+    private ExecutorService callbackThreadPool;
 
     public DiscordManager(SkybaneBot plugin) {
         super(plugin);
@@ -39,10 +63,63 @@ public class DiscordManager extends AbstractService {
 
     @Override
     public void onStart() {
+        callbackThreadPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors(), pool -> {
+            final ForkJoinWorkerThread worker = ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool);
+            worker.setName("SkybaneBot - JDA Callback " + worker.getPoolIndex());
+            return worker;
+        }, null, true);
 
+        final ThreadFactory gatewayThreadFactory = new ThreadFactoryBuilder().setNameFormat("SkybaneBot - JDA Gateway").build();
+        final ScheduledExecutorService gatewayThreadPool = Executors.newSingleThreadScheduledExecutor(gatewayThreadFactory);
+
+        final ThreadFactory rateLimitThreadFactory = new ThreadFactoryBuilder().setNameFormat("DiscordSRV - JDA Rate Limit").build();
+        final ScheduledExecutorService rateLimitThreadPool = new ScheduledThreadPoolExecutor(5, rateLimitThreadFactory);
+
+        try {
+            jda = JDABuilder.create(Sets.immutableEnumSet(intents))
+                    .setCallbackPool(callbackThreadPool, false)
+                    .setGatewayPool(gatewayThreadPool, true)
+                    .setRateLimitPool(rateLimitThreadPool, true)
+                    .setWebsocketFactory(new WebSocketFactory().setDualStackMode(DualStackMode.IPV4_ONLY))
+                    .setAutoReconnect(true)
+                    .setBulkDeleteSplittingEnabled(false)
+                    .setToken(botToken)
+                    .setContextEnabled(false)
+                    .build().awaitReady();
+        } catch (LoginException | InterruptedException ignored) {
+            // already logged by JDA
+        }
     }
 
     @Override
     public void onStop() {
+        final ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("SkybaneBot - Shutdown").build();
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        try {
+            executor.invokeAll(Collections.singletonList(() -> {
+                jda.getEventManager().getRegisteredListeners().forEach(l -> jda.getEventManager().unregister(l));
+
+                CompletableFuture<Void> shutdownTask = new CompletableFuture<>();
+                jda.addEventListener(new ListenerAdapter() {
+                    @Override
+                    public void onShutdown(ShutdownEvent event) {
+                        shutdownTask.complete(null);
+                    }
+                });
+                jda.shutdownNow();
+                jda = null;
+                try {
+                    shutdownTask.get(5, TimeUnit.SECONDS);
+                } catch(TimeoutException e) {
+                    logger.warning("JDA took too long to shut down!");
+                }
+
+                callbackThreadPool.shutdownNow();
+                return null;
+            }));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executor.shutdownNow();
     }
 }
